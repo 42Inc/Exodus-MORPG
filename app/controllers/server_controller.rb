64b6531c33cf3ec1226cd31261c4124854e3b069
Alexey R <alexey_r.98@ngs.ru>
2018-11-02 01:05:53 +0700

    Interface prototype?

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -1,8 +1,21 @@
 class ServerController < ApplicationController
   public
+    def initialize
+      @controller = "Server"
+      @page = "Default"
+    end
+    
     def info
+      @page = "Info"
+    end
+
+    def mainpage
+      @page = "Main"
     end
 
     def admconsole
+      @page = "Admin"
+      @r_ip = request.remote_ip
+      @r_port = request.port
     end
 end
