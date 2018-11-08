a0364a3f99eaaac6f4b891ffed94200fc2d41e6e
Alexey R <alexey_r.98@ngs.ru>
2018-11-09 01:35:48 +0700

    Started login|logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/game' => "game#game"
   get '/game/about' => "game#about"
   resources :users
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/server_controller/server', to: 'server#server'
 end
