d3d3da4122616f9a95a557d04de2035ec1820a17
Alexey R <alexey_r.98@ngs.ru>
2018-11-08 16:44:28 +0700

    Registration template. Test fail!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,51 @@
 class UsersController < ApplicationController
   attr_accessor :name, :email
-  def initialize
-    super
-    @controller = "Users"
-    @page = "Default"
-  end
+  public
+    def initialize
+      super
+      @controller = "Users"
+      @page = "Default"
+    end
 
-  def show
-    @page = "User"
-    @user = User.find(params[:id])
-  end
+    def show
+      @page = "User"
+      @user = User.find(params[:id])
+    end
 
-  def new
-    @page = "New"
-    @user = User.new
-  end
+    def new
+      @page = "New"
+      @user = User.new
+    end
 
-  def index
-    @page = "Users"
-  end
+    def index
+      @page = "Users"
+    end
 
-  def create
-    @page = "Create"
-  end
+    def create
+      @page = "Create"
+      @user = User.new(params_for_user)
+      if @user.save
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
 
-  def edit
-    @page = "Edit"
-  end
+    def edit
+      @page = "Edit"
+    end
 
-  def update
-    @page = "Update"
-  end
+    def update
+      @page = "Update"
+    end
 
-  def destroy
-    @page = "Delete"
-  end
+    def destroy
+      @page = "Delete"
+    end
+
+  private
+    def params_for_user
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- test/controllers/server_controller_test.rb
+++ test/controllers/server_controller_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class ServerControllerTest < ActionDispatch::IntegrationTest
   test "should get server" do
-    get server_url
+    get '/server'
     assert_response :success
     assert_select "title", "Server : Server"
   end
 
   test "should get info" do
-    get server_info_url
+    get '/server/info'
     assert_response :success
     assert_select "title", "Server : Info"
   end
 
   test "should get main" do
-    get root_url
+    get '/'
     assert_response :success
     assert_select "title", "Server : Main"
   end
 
   test "should get admconsole" do
-    get server_admconsole_url
+    get '/server/admconsole'
     assert_response :success
     assert_select "title", "Server : Admin"
   end
