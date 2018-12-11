e6c7b63028eb4eddad6acc63f54a22c2ac6da209
Alexey R <alexey_r.98@ngs.ru>
2018-12-12 01:12:16 +0700

    Started develop of game process

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,19 +7,29 @@ class GameController < ApplicationController
     @links_main_menu = []
     #link_name, link_controller, link_action
     @links_navigation_menu = []
+    @game_configuration = load_yml("game_config/game_configuration.yml")
   end
 
   def game
-    @links_main_menu = ["Start", "game", "gameprocess",
+    @links_main_menu = ["Start", "game", "play",
                         "About Game", "game", "about"]
     @links_navigation_menu = ["Main", "server", "main"]
     @page = "Game"
   end
 
-  def gameprocess
+  def play
     @links_navigation_menu = ["Main", "server", "main",
                               "Game", "game", "game"]
-    @page = "Gameprocess"
+    @page = "Play"
+    println(@game_configuration)
+  end
+
+  def game_iframes
+    if (params[:id] == "1")
+      render '/game/game_configuration_list.html.erb'
+    else
+      render '/server/notfound'
+    end
   end
 
   def about
