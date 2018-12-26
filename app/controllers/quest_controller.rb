bebee3ad685d9d9ae5d13d8fc0e5420d71c99f5f
Doshinsky <akter1997@gmail.com>
2018-12-26 23:16:19 +0700

    Fixed some strange sh*t, dunno why I added it -_-

--- app/controllers/quest_controller.rb
+++ app/controllers/quest_controller.rb
@@ -3,7 +3,7 @@ require "yaml"
 class QuestController < ApplicationController
   public
     def initialize
-      @eventList = YAML.load_file('db/quests.yml')
+      @eventList = YAML.load_file('config/quests.yml')
       @events_menu_variable = 0
       @event_count = 8
       @print_action_index = Array.new(@event_count).map!{Array.new(2)}
@@ -13,7 +13,7 @@ class QuestController < ApplicationController
     end
 
     def parser
-      statesSamples = ['name', 'HP', 'DMG', 'ARM', 'MP', 'ST', 'San', 'Cash']
+      statesSamples = ['name', 'HP', 'DMG', 'ARM', 'MP', 'ST', 'SAN', 'LCK', 'CASH']
       @actionsList = Array.new(@event_count).map!{Array.new(6)}
       counter_i = 0
       counter_j = 0
@@ -25,7 +25,6 @@ class QuestController < ApplicationController
           end
         end
       end
-      print_system("Triggering events...\n")
     end
 
     def get_event_for_hero(hero)
@@ -41,45 +40,16 @@ class QuestController < ApplicationController
         else
           @events_menu_variable = -2
         end
-
-      break if (test_events_menu_variable() != -1)
-        system "clear"
-        set_color("error")
-        STDOUT.print "[ERROR]  Incorrect option! Try again...\n\n"
-        set_color("default")
-      end
-
-      system "clear"
-
-      if (@accept_event_index == -1)
-        case @events_menu_variable
-          when -1
-            system "clear"
-            print_system("Exit game...\n")
-          when @print_action_count
-            system "clear"
-            print_system("Saving game...\n")
-          when @print_action_count + 1
-            system "clear"
-            print_system("Loading game...\n")
-            hero.load_hero('saves/stats.yml')
-          else
-            print_error("Method get_event_for_hero() -> switch-case error!\n")
-        end
-      else
        @events_menu_variable = accept_event_for_hero(hero)
       end
       return @events_menu_variable
     end
 
     def print_events_for_hero(hero)
-      STDOUT.print("Select\n")
-      STDOUT.print("Quit game: [0]\n")
       @print_action_count = 0
       offset = 0
       for counter_i in 0..(@event_count - 1)
         if (!((@actionsList[counter_i][0] == "Go to the mines") && hero.mp < 50 && hero.san < 10))
-          STDOUT.print("Event available: [" + (counter_i + 1 - offset).to_s  + "] "  + @actionsList[counter_i][0] + "\n")
           @print_action_index[counter_i][0] = counter_i - offset
           @print_action_index[counter_i][1] = counter_i
           @print_action_count = @print_action_count + 1
@@ -89,9 +59,6 @@ class QuestController < ApplicationController
           offset = offset + 1
         end
       end
-
-      STDOUT.print("Save game: [" + (@event_count - offset + 1).to_s + "]\n")
-      STDOUT.print("Load game: [" + (@event_count - offset + 2).to_s + "]\n")
     end
 
     def test_events_menu_variable()
@@ -116,13 +83,13 @@ class QuestController < ApplicationController
       old_mp = hero.mp
       old_st = hero.st
       old_san = hero.san
+      old_lck = hero.lck
       old_cash = hero.cash
 
       if (@actionsList[@accept_event_index][1] != nil)
         hero.hp = hero.hp + @actionsList[@accept_event_index][1].to_i
         hero.hp = hero.hp > 100 ? 100 : hero.hp
         unless hero.hp > 0
-          print_error_msg("WASTED!\n")
           accept_error = 1
           game_over = -2
         else
@@ -133,7 +100,7 @@ class QuestController < ApplicationController
         hero.mp = hero.mp + @actionsList[@accept_event_index][2].to_i
         hero.mp = hero.mp > 100 ? 100 : hero.mp
         unless hero.mp > 0
-          print_error_msg("Not enough mana!\n")
+          # print_error_msg("Not enough mana!\n")
           accept_error = 1
         else
           hero.mp = hero.mp
@@ -143,7 +110,7 @@ class QuestController < ApplicationController
         hero.st = hero.st + @actionsList[@accept_event_index][3].to_i
         hero.st = hero.st > 100 ? 100 : hero.st
         unless hero.st > 0
-          print_error_msg("Not allowed\n")
+          # print_error_msg("Not allowed\n")
           accept_error = 1
         else
           hero.st = hero.st
@@ -154,7 +121,7 @@ class QuestController < ApplicationController
         hero.san = hero.san > 10 ? 10 : hero.san
         unless hero.san > -10
           hero.san = -10
-          print_error_msg("Your hero just commited suicide!\n")
+          # print_error_msg("Your hero just commited suicide!\n")
           game_over = -2
         else
           hero.san = hero.san
@@ -164,7 +131,7 @@ class QuestController < ApplicationController
         hero.cash = hero.cash + @actionsList[@accept_event_index][5].to_i
         if (hero.cash < 0)
           accept_error = 1
-          print_error_msg("Nischebrod!\n")
+          # print_error_msg("Nischebrod!\n")
         end
       end
 
@@ -177,7 +144,7 @@ class QuestController < ApplicationController
           hero.san = hero.san - 3
           unless hero.san > -10
             hero.san = -10
-            print_error_msg("Your hero just commited suicide!\n")
+            # print_error_msg("Your hero just commited suicide!\n")
             game_over = -2
           else
             hero.san = hero.san
@@ -190,6 +157,7 @@ class QuestController < ApplicationController
         hero.mp = old_mp
         hero.st = old_st
         hero.san = old_san
+        hero.lck = old_lck
         hero.cash = old_cash
       end
       return game_over
